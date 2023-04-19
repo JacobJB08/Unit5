3e9838409d3084fb4af21bf953bb1a076d151cf6
JacobJB08 <721378@student.dusd.net>
2023-04-19 09:57:42 -0700

    a lot

--- Unit5Project/Assets/Scripts/Target.cs
+++ Unit5Project/Assets/Scripts/Target.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
-    private float minSpeed = 12;
-    private float maxSpeed = 16;
+    private float minSpeed = 14f;
+    private float maxSpeed = 16.5f;
     private float maxTorque = 10;
-    private float xRange = 4;
-    private float ySpawnPos = -6;
+    private float xRange = 5;
+    private float ySpawnPos = -4;
+    private GameManager gameManager;
+    public int pointValue;
+    public ParticleSystem explosionParticle;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +21,7 @@ public class Target : MonoBehaviour
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
         transform.position = RandomSpawnPos();
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     Vector3 RandomForce()
@@ -38,5 +42,17 @@ public class Target : MonoBehaviour
     void Update()
     {
         
+    }
+    
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+        gameManager.UpdateScore(pointValue);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
     }
 }
