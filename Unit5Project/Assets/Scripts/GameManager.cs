86b3089a0a06df7648253f93b26396eaca08c88b
JacobJB08 <721378@student.dusd.net>
2023-04-25 09:58:52 -0700

    cool

--- Unit5Project/Assets/Scripts/GameManager.cs
+++ Unit5Project/Assets/Scripts/GameManager.cs
@@ -8,20 +8,22 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
-    private float spawnRate = 1.0f;
+    private float spawnRate = 2f;
     private int score;
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
     public bool isGameActive;
     public Button restartButton;
+    public GameObject titleScreen;
+    public GameObject point;
+    public GameObject display1;
+    public TextMeshProUGUI livesText;
+    private int lives;
 
     // Start is called before the first frame update
     void Start()
     {
-        isGameActive = true;
-        StartCoroutine(SpawnTarget());
-        score = 0;
-        UpdateScore(0);
+
     }
 
     public void GameOver()
@@ -53,8 +55,31 @@ public class GameManager : MonoBehaviour
         scoreText.text = "Score: " + score;
     }
 
+    public void UpdateLives(int livesToChange)
+    {
+        lives += livesToChange;
+        livesText.text = "Lives: " + lives;
+        if (lives <= 0)
+        {
+            GameOver();
+        }
+    }
+
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartGame(int difficulty)
+    {
+        isGameActive = true;
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
+        UpdateLives(3);
+        titleScreen.gameObject.SetActive(false);
+        spawnRate /= difficulty;
+        point.gameObject.SetActive(true);
+        display1.gameObject.SetActive(true);
     }
 }
